64909ac22c77736547753368d50a92c109956ae2
jrapier <101356944+jrapier-designs@users.noreply.github.com>
2022-04-26 17:56:37 -0400

    Nextjs and Shopify setup
    nextjs and shopify project setup

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+// import App from 'next/app'
+// import '../globals.css'
+import "tailwindcss/tailwind.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
